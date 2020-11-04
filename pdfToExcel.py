95ee09da5a8504ae3f136fe38bc848619cd7ccbe
Kim-Erik Kalsnes <KIK@equinor.com>
2020-11-04 12:51:41 +0100

    Tried other ways to convert PDF to excel. NO CLUE!!

--- pdfToExcel.py
+++ pdfToExcel.py
@@ -1,10 +1,25 @@
 import tabula
 import pandas as pd
+import os
 
-#tabula.convert_into("MSA_linje 1_1.pdf", "output2.csv", output_format="tsv", pages='all')
 
-df = tabula.read_pdf('MSA_linje 1_1.pdf', pages = 'all')[0]
+# -----[ Global Variables ]-----
+pdffiles = []
+directory = ("F:\\Scripts\\Kfaktorlogg\\MSA_1")
+
+# Make list of pdf files
+for fileName in os.listdir(directory):
+    if fileName.endswith(".pdf"):
+        pdffiles.append(fileName)
+
+print(pdffiles)
+#tabula.convert_into(pdffiles, directory, output_format="tsv", pages='all')
+#print(path)
+
+#tabula.convert_into_by_batch(inputDir, output_format="tsv", pages='all')
+
+#df = tabula.read_pdf('MSA_linje 1_1.pdf', pages = 'all')[0]
 #df.values = [x.replace(',', '.') for x in df[values]]
 #df.to_tsv('test.csv', sep=' ', encoding='utf-8', quotechar='"', decimal=',')
-print(df.values)
+#print(df.values)
 
